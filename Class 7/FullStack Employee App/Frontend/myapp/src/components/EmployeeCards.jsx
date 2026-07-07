4341faa6ebb0c8c59b0b42b0d382a07455d32c04
Pratik Raj Verma <erpratikrajverma@gmail.com>
2026-07-07 11:10:51 +0530

    frontend Backend Interagration

---
+++ Class 7/FullStack Employee App/Frontend/myapp/src/components/EmployeeCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmployeeCards = () => {
+  return (
+    <div>
+        <h1>Employee System App</h1>
+        <form>
+            <input placeholder='Name'/><br/>
+            <input placeholder='Email'/><br/>
+            <input placeholder='Emp. Id'/><br/>
+            <button>Sumbit</button>
+        </form>
+    </div>
+  )
+}
+
+export default EmployeeCards
---
+++ Class 7/FullStack Employee App/Frontend/myapp/src/service/api.js
@@ -0,0 +1,11 @@
+const backendApiUrl = 'http://localhost:4000'
+
+const createuser = `${backendApiUrl}/createuser`
+
+const getuser = `${backendApiUrl}/getuser`
+
+const updateuser = `${backendApiUrl}/updateuser`
+
+const deleteuser = `${backendApiUrl}/deleteuser`
+
+export {createuser, getuser, updateuser, deleteuser}
